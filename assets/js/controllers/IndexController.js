3a8b2573129be7444ea08cac3ec29828b767e7e8
Joseph Felix <contato@josephfelix.dev>
2021-04-13 11:27:08 -0300

    Added facebook integration to home

--- assets/js/controllers/IndexController.js
+++ assets/js/controllers/IndexController.js
@@ -1,14 +1,64 @@
 angular.module("supertests").controller("IndexController", [
     "$scope",
     "$http",
-    function ($scope, $http) {
+    "$window",
+    "Facebook",
+    function ($scope, $http, $window, Facebook) {
         var limit = 30;
+
         $scope.testes = [];
+
         $scope.loading = true;
+
         $scope.init = function () {
             $http.get("/l").then(function (json) {
                 $scope.loading = false;
                 $scope.testes = json.data;
+            });
+        };
+
+        /**
+         * Realiza o login no facebook via JS SDK
+         */
+        $scope.loginFacebook = function () {
+            Facebook.login(
+                function (response) {
+                    if (response.authResponse) {
+                        $scope.loginSite();
+                    }
+                },
+                { scope: "email" }
+            );
+        };
+
+        /**
+         * Realiza o login no site
+         * @param guid
+         */
+        $scope.loginSite = function (guid) {
+            var fields = ["id", "name", "email", "picture"];
+
+            Facebook.api("/me?fields=" + fields.join(","), function (response) {
+                var loginSuccess = function (result) {
+                    if (!result.status) {
+                        alert(result.result);
+                    } else {
+                        $window.location.reload();
+                    }
+                };
+
+                var loginError = function () {
+                    alert("Ocorreu um erro ao fazer login, tente novamente.");
+                };
+
+                var userData = {
+                    id: response.id,
+                    name: response.name,
+                    email: response.email,
+                    photo: response.picture.data.url,
+                };
+
+                $http.post("/login", userData).then(loginSuccess, loginError);
             });
         };
     },
